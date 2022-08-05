d6a4f20a0023d3d385f2bc7fe88f6c83db3ce764
MACEDO <macedo.gti@gmail.com>
2022-08-05 10:27:10 -0300

    style: :sparkles: new layout
    disable guest and stores while adjustments layou admin

--- layouts/restrict.vue
+++ layouts/restrict.vue
@@ -4,27 +4,30 @@
     <div class="py-6 px-6">
       <nuxt />
     </div>
+    <AdminBottom class="sm:invisible md:invisible lg:invisible xl:md:invisible" />
   </div>
 </template>
 
 <script>
 import AdminHeader from "@/components/layout/AdminHeader.vue";
+import AdminBottom from "@/components/layout/AdminBottom.vue";
 export default {
   name: "restrict",
-  middleware: ["guest"],
+  // middleware: ["guest"],
   components: {
     AdminHeader,
+    AdminBottom,
   },
   data() {
     return {};
   },
 
-  mounted() {
-    if (this.$auth.loggedIn == true) {
-      this.$store.dispatch("auth-user/SET_USER_LOGGED", true);
-    } else {
-      this.$store.dispatch("auth-user/SET_USER_LOGGED", false);
-    }
-  },
+  // mounted() {
+  //   if (this.$auth.loggedIn == true) {
+  //     this.$store.dispatch("auth-user/SET_USER_LOGGED", true);
+  //   } else {
+  //     this.$store.dispatch("auth-user/SET_USER_LOGGED", false);
+  //   }
+  // },
 };
 </script>
